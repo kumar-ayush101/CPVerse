300266da9e62e5160b5d81e0de2d944fb811e01d
Ayush Kumar <sk5020188@gmail.com>
2025-06-23 10:38:07 +0530

    cookies settings changed in authController

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -86,11 +86,11 @@ export const login = async (req, res) => {
 
     
     res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', 
-      sameSite: 'strict',
-      maxAge: 24 * 60 * 60 * 1000 
-    });
+  httpOnly: true,
+  secure: true, 
+  sameSite: 'none', 
+  maxAge: 24 * 60 * 60 * 1000
+});
 
     return res.status(200).json({ message: "Login successful" });
 
