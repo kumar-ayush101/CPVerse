ae6d26be5751967d207418c90855b745e65b5b59
Ayush Kumar <sk5020188@gmail.com>
2025-07-08 13:10:28 +0530

    add localhost also for origin

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://cpverse.netlify.app',
+  origin: ['https://cpverse.netlify.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
